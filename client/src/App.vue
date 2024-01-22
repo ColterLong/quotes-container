a97667cb5520d06e8b605e2dc414f1d789c83183
ColterLong <colterlong@protonmail.com>
2024-01-22 14:37:24 -0600

    working docker compose with front end back end with reverse proxy

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,43 +7,42 @@ const quotes = ref([]);
 const loading = ref(true);
 
 onMounted(() => {
-  // axios.get('api/getQuotes')
-  //   .then((res) => {
-  //     // console.log('hope there are quotes below')
-  //     // console.log(res.data)
-  //     quotes.value = res.data;
-  //   })
-  //   .catch((err) => {
-  //     console.error('Error getting quotes: ', err);
-  //   })
-  //   // eslint-disable-next-line no-return-assign
-  //   .finally(() => (loading.value = false));
+  axios.get('api/getQuotes')
+    .then((res) => {
+      // console.log('hope there are quotes below')
+      // console.log(res.data)
+      quotes.value = res.data;
+    })
+    .catch((err) => {
+      console.error('Error getting quotes: ', err);
+    })
+    // eslint-disable-next-line no-return-assign
+    .finally(() => (loading.value = false));
 
-  quotes.value = [{
-    name: 'test 1',
-    text: 'quote text 1',
-  }, {
-    name: 'test 2',
-    text: 'quote text 2',
-  }];
-  loading.value = false;
+  // quotes.value = [{
+  //   name: 'test 1',
+  //   text: 'quote text 1',
+  // }, {
+  //   name: 'test 2',
+  //   text: 'quote text 2',
+  // }];
+  // loading.value = false;
 });
 </script>
 
 <template>
-    <h1 class="title">Top 5 Famous Quotes</h1>
-    <div v-if="loading">
-      <Quote :name="''" :text="'Loading...'" />
+  <h1 class="title">Top 5 Famous Quotes</h1>
+  <div v-if="loading">
+    <Quote :name="''" :text="'Loading...'" />
+  </div>
+  <div v-else>
+    <div class="quotes" v-for="quote in quotes" :key="quote.name">
+      <Quote :name="quote.name" :text="quote.text" />
     </div>
-    <div v-else>
-      <div class="quotes" v-for="quote in quotes" :key="quote.name">
-        <Quote :name="quote.name" :text="quote.text" />
-      </div>
-    </div>
+  </div>
 </template>
 
 <style scoped>
-
 .title {
   color: #ececec;
   max-width: 800px;
